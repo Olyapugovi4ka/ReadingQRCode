6656845c3b9184d99bb9ba3816bd313fcba181fb
Olyapugovi4ka <olyapugovi4ka@gmail.com>
2019-12-24 19:05:50 +0300

    create the project

---
+++ ReadingQRCode/ReadingQRCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadingQRCode
+//
+//  Created by Olga Melnik on 24.12.2019.
+//  Copyright © 2019 Olga Melnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
